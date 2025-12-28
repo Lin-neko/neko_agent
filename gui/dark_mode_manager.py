c19c7d5b51d3d19dbe10fdf08274897272430ca2
Lin-neko <1844683904@qq.com>
2025-12-28 18:47:56 +0800

    Revert "深色模式设置可用"
    This reverts commit fc75221f3702c9a5b19eec0033f9a8d3ff31fe7a.

--- gui/dark_mode_manager.py
+++ gui/dark_mode_manager.py
@@ -1,9 +1,6 @@
 from PIL import ImageGrab
-import json
+
 def dark_or_light(x, y, threshold = 128):
-    with open("config.json", "r", encoding='utf-8') as f:
-        configs = json.load(f)
-        dark_conf = configs["dark_mode"]
     try:
         screenshot = ImageGrab.grab()
         pixel_color = screenshot.getpixel((x, y))
@@ -11,12 +8,7 @@ def dark_or_light(x, y, threshold = 128):
         print(f"huh?获取像素过程似乎出了点问题哦: {e}")              
     # 计算亮度 
     luminance = (0.299 * pixel_color[0] + 0.587 * pixel_color[1] + 0.114 * pixel_color[2])
-    if dark_conf == "自动" :
-        if luminance < threshold :
-            return "Light"
-        else :
-            return "Dark"
-    elif dark_conf == "始终深色":
-        return "Dark"
-    else :
+    if luminance < threshold :
         return "Light"
+    else :
+        return "Dark"
